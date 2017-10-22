9ef7ca6a330b4b561bdea15ddd23f7f97060be93
Paul Robertson <paul.robertson@place4waste.com>
2017-10-22 22:26:17 +0100

    Update numberwords2.hs

--- numberwords2.hs
+++ numberwords2.hs
@@ -1,19 +1,16 @@
 import Test.QuickCheck
 import Data.List
 import Data.Char
-import qualified Data.Map as Map
 import Data.Maybe
-
-numberWordList :: [(Int,String)]
-numberWordList = [(0,""),(1,"one"),(2,"two"),(3,"three"),(4,"four"),(5,"five"),(6,"six"),(7,"seven"),(8,"eight"),(9,"nine"),(10,"ten"),(11,"eleven"),(12,"twelve"),(13,"thirteen"),(14,"fourteen"),(15,"fifteen"),(16,"sixteen"),(17,"seventeen"),(18,"eightteen"),(19,"nineteen"),(20,"twenty"),(30,"thirty"),(40,"forty"),(50,"fifty"),(60,"sixty"),(70,"seventy"),(80,"eighty"),(90,"ninety")]
 
 -- association lists
 -- map numbers to words
-numberWords :: Map.Map Int String
-numberWords = Map.fromList numberWordList
+numberWords :: [(Int,String)]
+numberWords = [(0,""),(1,"one"),(2,"two"),(3,"three"),(4,"four"),(5,"five"),(6,"six"),(7,"seven"),(8,"eight"),(9,"nine"),(10,"ten"),(11,"eleven"),(12,"twelve"),(13,"thirteen"),(14,"fourteen"),(15,"fifteen"),(16,"sixteen"),(17,"seventeen"),(18,"eightteen"),(19,"nineteen"),(20,"twenty"),(30,"thirty"),(40,"forty"),(50,"fifty"),(60,"sixty"),(70,"seventy"),(80,"eighty"),(90,"ninety")]
+
 -- map words to numbers
-wordNumbers :: Map.Map String Int
-wordNumbers = Map.fromList $ map (\(x,y) -> (y,x)) numberWordList
+wordNumbers :: [(String,Int)]
+wordNumbers = map (\(x,y) -> (y,x)) numberWords
 
 --------------------------------------------------------------------------------------------------------
 --
@@ -41,7 +38,7 @@ toWords' n
   | otherwise = toWords'' n
 
 toWords'' :: Int -> String
-toWords'' n = if (Map.lookup n numberWords) /= Nothing then fromJust (Map.lookup n numberWords) else ""
+toWords'' n = maybe "" id (lookup n numberWords)
 
 --------------------------------------------------------------------------------------------------------
 --
@@ -94,7 +91,7 @@ htuToNumber ws = 100 * (toNumber'' $ head' $ huns) + (toNumber'' $ head' $ tens)
         head' (x:xs) = x
 
 toNumber'' :: String -> Int
-toNumber'' s = if (Map.lookup s wordNumbers) /= Nothing then fromJust (Map.lookup s wordNumbers) else 0
+toNumber'' s = maybe 0 id (lookup s wordNumbers)
 
 -- we can use QuickCheck to test a bunch of numbers, converted to words and then back to numbers
 prop_there_and_back n = (toNumber . toWords) (abs n) == (abs n)
